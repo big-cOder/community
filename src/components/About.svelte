b7bd28342e08b40fe03cc2529a5788e6bccf9f6c
DanielArmR <daniel62armas@gmail.com>
2021-04-15 21:24:55 -0500

    Build About component

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -3,9 +3,29 @@
 </script>
 
 <section>
-    <p>Hola</p>
+    <h1>Sobre Nosotros</h1>
+    <p>Somos una Comunidad de estudiantes y desarrolladores apasionados por el gran mundo de la tecnología, nos apoyamos mutuamente para poder aprender y desarrollar grandes cosas.</p>
 </section>
 
 <style>
+    section{
 
+        width: 40%;
+        background-color: #000000;
+        border-radius: 5px;
+        font-family: monospace;
+        padding: 4rem 3rem;
+    }
+    h1{
+        font-size: 4rem;
+        color: #fff;
+    }
+    h1::before{
+        content: ">_";
+    }
+    p{
+        padding: 2rem;
+        font-size: 1.6rem;
+        color: #efefef;
+    }
 </style>
